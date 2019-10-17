a1a26502389b20a6f5bfba5e637d44c791f599d4
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2019-10-17 21:19:48 +0300

    Change the dashboard domain to be separate

--- manifests/kubernetes-dashboard/values.js
+++ manifests/kubernetes-dashboard/values.js
@@ -3,7 +3,7 @@ import { valuesPipeForFunctions, valuesMutator } from "../../jkcfg/util"
 function withCustomIngressHost() {
     return valuesMutator(function(values) {
         values.ingress.hosts = [
-            "cluster-" + values.workshopctl.clusterNumber + "." + values.workshopctl.domain
+            "dashboard.cluster-" + values.workshopctl.clusterNumber + "." + values.workshopctl.domain
         ]
         return values
     })
