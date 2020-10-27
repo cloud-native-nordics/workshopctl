abfee59ce1a69d9f9699359d09c39554a9bd406a
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2020-10-27 20:42:50 +0200

    Upgrade code-server to latest version and slightly adapt the scripts and configuration

--- images/k8s-web-ide/entrypoint.sh
+++ images/k8s-web-ide/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [[ ${GIT_REPO} != "" ]]; then
-    git clone --depth 5 ${GIT_REPO} /home/coder/gitclone
-    mv /home/coder/gitclone/${GIT_SUBDIR}/* /home/coder/project
+if [[ ${TUTORIALS_REPO} != "" ]]; then
+    # This will do a quick, shallow clone of the repo
+    git clone --depth 1 ${TUTORIALS_REPO} /home/coder/gitclone
+    # If TUTORIALS_DIR is "." or "", this will copy the whole git repo.
+    mkdir -p /home/coder/project
+    mv /home/coder/gitclone/${TUTORIALS_DIR}/* /home/coder/project
+    sudo rm -r /home/coder/gitclone
+    echo "Initialized workspace content from git repo ${TUTORIALS_REPO} with subdir ${TUTORIALS_DIR}"
 fi
 
 sudo chown $(id -u):$(id -g) /var/run/docker.sock
 
-exec code-server --host=0.0.0.0 --auth=password --disable-telemetry
+# By default run behind a Let's Encrypt proxy, so expose this traffic using insecure HTTP
+exec code-server --host=0.0.0.0 --auth=password --disable-telemetry /home/coder/project
