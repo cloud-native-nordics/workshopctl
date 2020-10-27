fe06e43a7fdc352b1175f377a771cf91af0bbc86
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2020-10-28 01:35:27 +0200

    Add dry-run handling to contexts

--- pkg/util/context.go
+++ pkg/util/context.go
@@ -6,12 +6,34 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func NewContext(dryRun bool) context.Context {
+	return WithDryRun(context.Background(), dryRun)
+}
+
 var clusterNumberKey = clusterNumberKeyImpl{}
 
 type clusterNumberKeyImpl struct{}
 
 func WithClusterNumber(ctx context.Context, n uint16) context.Context {
 	return context.WithValue(ctx, clusterNumberKey, n)
+}
+
+var dryRunKey = dryRunKeyImpl{}
+
+type dryRunKeyImpl struct{}
+
+func WithDryRun(ctx context.Context, dryRun bool) context.Context {
+	return context.WithValue(ctx, dryRunKey, dryRun)
+}
+
+func IsDryRun(ctx context.Context) bool {
+	dryRun, ok := ctx.Value(dryRunKey).(bool)
+	if !ok {
+		logrus.Warn("Expected to be able to get dry-run from context, but got nothing")
+		logrus.Warn("Setting dryRun to be true because of this")
+		dryRun = true
+	}
+	return dryRun
 }
 
 func Logger(ctx context.Context) *logrus.Entry {
