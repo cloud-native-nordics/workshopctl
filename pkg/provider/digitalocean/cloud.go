8234a702f2d1460f1d186e1f878a7d2ecc364a1a
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2020-10-28 02:47:52 +0200

    Implement DO delete cluster

--- pkg/provider/digitalocean/cloud.go
+++ pkg/provider/digitalocean/cloud.go
@@ -3,18 +3,22 @@ package digitalocean
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 	"net/url"
 	"time"
 
 	"github.com/cloud-native-nordics/workshopctl/pkg/config"
+	"github.com/cloud-native-nordics/workshopctl/pkg/constants"
 	"github.com/cloud-native-nordics/workshopctl/pkg/provider"
 	"github.com/cloud-native-nordics/workshopctl/pkg/util"
 	"github.com/digitalocean/godo"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 )
+
+var clusterNotFound = fmt.Errorf("couldn't find cluster by name")
 
 const (
 	DefaultRegion  = "fra1"
@@ -71,15 +75,15 @@ func chooseSize(c config.NodeClaim) string {
 	return "s-2vcpu-4gb"
 }
 
-func (do *DigitalOceanCloudProvider) CreateCluster(ctx context.Context, i config.ClusterNumber, c provider.ClusterSpec) (*provider.Cluster, error) {
+func (do *DigitalOceanCloudProvider) CreateCluster(ctx context.Context, c provider.ClusterSpec) (*provider.Cluster, error) {
 	logger := util.Logger(ctx)
 
 	start := time.Now().UTC()
+	i := c.Index
 	cluster := &provider.Cluster{
 		Spec: c,
 		Status: provider.ClusterStatus{
 			ProvisionStart: &start,
-			Index:          i,
 		},
 	}
 
@@ -98,9 +102,9 @@ func (do *DigitalOceanCloudProvider) CreateCluster(ctx context.Context, i config
 	}
 
 	req := &godo.KubernetesClusterCreateRequest{
-		Name:        c.Name,
+		Name:        cluster.Spec.Name(),
 		RegionSlug:  do.region,
-		VersionSlug: c.Version, // TODO: Resolve c.Version correctly
+		VersionSlug: cluster.Spec.Version, // TODO: Resolve c.Version correctly
 		Tags: []string{
 			WorkshopctlTag,
 		},
@@ -112,30 +116,28 @@ func (do *DigitalOceanCloudProvider) CreateCluster(ctx context.Context, i config
 		b, _ := json.Marshal(req)
 		if do.dryRun {
 			log.Infof("Would send this request to DO: %s", string(b))
+			// TODO: Revamp this dry-run logic and unify it with DebugObject
 			return cluster, nil
 		}
 		log.Debugf("Would send this request to DO: %s", string(b))
 	}
 	// TODO: Rate limiting
-	clusters, _, err := do.c.Kubernetes.List(ctx, &godo.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-	for _, c := range clusters {
-		if c.Name == cluster.Spec.Name {
-			logger.Infof("Found existing cluster with name %q and ID %q", c.Name, c.ID)
-			cluster.Status.ID = c.ID
-			break
-		}
-	}
+	doCluster, err := do.getClusterByName(ctx, cluster.Spec.Name())
+	if err == nil {
+		// If the cluster was found, just note it's ID
+		cluster.Status.ID = doCluster.ID
+		logger.Infof("Found existing cluster with name %q and ID %q", cluster.Spec.Name(), cluster.Status.ID)
 
-	if len(cluster.Status.ID) == 0 {
-		logger.Infof("Creating new cluster with name %s", cluster.Spec.Name)
-		doCluster, _, err := do.c.Kubernetes.Create(ctx, req)
+	} else if errors.Is(err, clusterNotFound) {
+		// If the cluster wasn't found, create it
+		logger.Infof("Creating new cluster with name %s", cluster.Spec.Name())
+		doCluster, _, err = do.c.Kubernetes.Create(ctx, req)
 		if err != nil {
 			return nil, err
 		}
 		cluster.Status.ID = doCluster.ID
+	} else { // unexpected err != nil
+		return nil, err
 	}
 
 	err = util.Poll(ctx, nil, func() (bool, error) {
@@ -143,7 +145,7 @@ func (do *DigitalOceanCloudProvider) CreateCluster(ctx context.Context, i config
 		if err != nil {
 			return false, fmt.Errorf("getting a kubernetes cluster failed: %v", err)
 		}
-		util.DebugObject("Got Kubernetes cluster response from DO", kcluster)
+		util.DebugObject(ctx, "Got Kubernetes cluster response from DO", kcluster)
 
 		if kcluster.Status.State == godo.KubernetesClusterStatusRunning {
 			logger.Infof("Awesome, the cluster is Ready! Endpoints: %s %s", kcluster.Endpoint, kcluster.IPv4)
@@ -180,5 +182,50 @@ func (do *DigitalOceanCloudProvider) CreateCluster(ctx context.Context, i config
 }
 
 func (do *DigitalOceanCloudProvider) DeleteCluster(ctx context.Context, index config.ClusterNumber) error {
-	return nil // TODO: implement this
+	name := constants.ClusterName(index)
+
+	cluster, err := do.getClusterByName(ctx, name)
+	if err != nil {
+		return err
+	}
+
+	util.DebugObject(ctx, "Found wanted cluster", cluster)
+
+	// TODO: Delete LBs
+
+	return do.deleteCluster(ctx, cluster)
+}
+
+func (do *DigitalOceanCloudProvider) getClusterByName(ctx context.Context, name string) (*godo.KubernetesCluster, error) {
+	logger := util.Logger(ctx)
+
+	logger.Debug("Listing Kubernetes clusters...")
+	clusters, _, err := do.c.Kubernetes.List(ctx, &godo.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, cluster := range clusters {
+		// Filter by name
+		if cluster.Name != name {
+			logger.Debugf("Cluster name %s isn't desired %s", cluster.Name, name)
+			continue
+		}
+		// Found it
+		return cluster, nil
+	}
+
+	return nil, fmt.Errorf("%w: %s", clusterNotFound, name)
+}
+
+func (do *DigitalOceanCloudProvider) deleteCluster(ctx context.Context, c *godo.KubernetesCluster) error {
+	logger := util.Logger(ctx)
+
+	if util.IsDryRun(ctx) {
+		logger.Infof("Would delete Kubernetes cluster %s", c.Name)
+		return nil
+	}
+	logger.Infof("Deleting Kubernetes cluster %s", c.Name)
+	_, err := do.c.Kubernetes.Delete(ctx, c.ID)
+	return err
 }
